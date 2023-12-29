00a32f7e84c12d6ac312f79dd311cd477b1dbef5
Adolphus Miracle <miraboy13@gmail.com>
2023-12-29 13:18:51 +0100

    feat: created fresh project and installed needed dependencies

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} className='bg-amber-400'>
+      <Text className='text-red-400'>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
