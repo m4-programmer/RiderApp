3787f11de4a36ed6997cf18c00bdcd167896573e
Adolphus Miracle <miraboy13@gmail.com>
2023-12-29 15:13:44 +0100

    refactor: added more rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
-     "./<custom directory>/**/*.{js,jsx,ts,tsx}"
+     "./src/screens/**/*.{js,jsx,ts,tsx}",
+     "./src/components/**/*.{js,jsx,ts,tsx}",
+     "./src/navigation/**/*.{js,jsx,ts,tsx}",
     ],
   theme: {
     extend: {},
