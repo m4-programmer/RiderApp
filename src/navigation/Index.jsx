e826f373a8ac80de92084f3800e476ba35e16b34
Adolphus Miracle <miraboy13@gmail.com>
2023-12-31 21:01:14 +0100

    feat: added new navigation screen

--- src/navigation/Index.jsx
+++ src/navigation/Index.jsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import WelcomeScreen from '../screens/WelcomeScreen'
 import OnboardingScreen from '../screens/OnboardingScreen'
+import GetYouInScreen from '../screens/SignUp/GetYouInScreen'
 
 const Stack = createNativeStackNavigator()
 
@@ -12,6 +13,7 @@ const AppNavigation = () => {
       <Stack.Navigator initialRouteName='Welcome' screenOptions={{headerShown: false}}>
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{gestureEnabled: false, gestureDirection: 'horizontal'}} />
+        <Stack.Screen name="GetYouIn" component={GetYouInScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
