0c02c5713cee46c94f4828ec751532644415f6ee
Adolphus Miracle <miraboy13@gmail.com>
2023-12-31 21:01:56 +0100

    feat: added logic to animate movement of onboarding screen using button

--- src/components/splash/SplashLayoutComponent.jsx
+++ src/components/splash/SplashLayoutComponent.jsx
@@ -4,16 +4,28 @@ import { StatusBar } from 'expo-status-bar'
 import HeadingComponent from './HeadingComponent'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import FooterComponent from './FooterComponent';
-const SplashLayoutComponent = ({splashData}) => {
- const {id,title, subTitle, mode, bgColor, img} = splashData
- 
+const SplashLayoutComponent = ({splashData, skip, navigation, next}) => {
+ const {id,title, subTitle, mode, bgColor, img, } = splashData
+ const CustomStatusBar = ({ mode }) => {
+    return (
+      <View>
+        {mode === 'dark' ? (
+          <StatusBar style="light"  />
+        ) : (
+          <StatusBar style="dark" />
+        )}
+      </View>
+    );
+  };
+  
   return (
 
-    <View className='flex-1 ' style={{backgroundColor: bgColor}}>
+    <View className='flex-1 ' style={{backgroundColor: bgColor, width: wp(100), height: '100%'}}>
+        <CustomStatusBar mode={mode} />
         {
             ( id === 1 || id === 2 )  ? 
-            <FirstDesign id={id} title={title} subTitle={subTitle} mode={mode} bgColor={bgColor} img={img}/> : 
-            <SecondDesign id={id} title={title} subTitle={subTitle} mode={mode} bgColor={bgColor} img={img}/> 
+            <FirstDesign id={id} title={title} subTitle={subTitle} mode={mode} bgColor={bgColor} img={img} next={next} skip={skip}/> : 
+            <SecondDesign id={id} title={title} subTitle={subTitle} mode={mode} bgColor={bgColor} img={img} next={next} skip={skip}/> 
         }
     </View>
 
@@ -21,11 +33,11 @@ const SplashLayoutComponent = ({splashData}) => {
 
 
 }
-const FirstDesign = ({id,title, subTitle, mode, img}) => {
+const FirstDesign = ({id,title, subTitle, mode, img, next, skip}) => {
     return (
             <SafeAreaView  style={{ flex: 1,flexDirection: 'column', justifyContent: 'space-between', }}>
                    <View className='mx-5 '>
-                        <StatusBar style={mode === 'dark' ? 'dark' : 'light'} />
+                        
                         <HeadingComponent title={title} subTitle={subTitle} mode={mode} paddingTop={hp(4.5)}/>
                    </View>
                    <View className='flex '>
@@ -39,22 +51,21 @@ const FirstDesign = ({id,title, subTitle, mode, img}) => {
                     </View>
 
                     <View className='mx-5' style={{ marginBottom: hp(2.0)}}>
-                        <FooterComponent id={id} mode={mode} />
+                        <FooterComponent id={id} skip={skip} next={next} mode={mode}/>
                     </View>
             </SafeAreaView>
     )
 }
 
-const SecondDesign = ({id,title, subTitle, mode, img}) => {
+const SecondDesign = ({id,title, subTitle, mode, img, next, skip}) => {
     return (
         <ImageBackground source={img} style={{ width: '100%', height: hp(100), resizeMode: 'contain' }}>
             <SafeAreaView className='mx-5' style={{ flex: 1, }}>
-                    <StatusBar style={mode === 'dark' ? 'dark' : 'light'} />
                     <HeadingComponent title={title} subTitle={subTitle} mode={mode} paddingTop={hp(4.5)}/>
 
                     <View style={{position: 'absolute', bottom: 0, width: '100%', marginBottom: hp(5)}}>
                         <>
-                            <FooterComponent id={id} />
+                            <FooterComponent id={id} skip={skip} next={next} mode={mode} />
                         </>
                     </View>
             </SafeAreaView>
--- src/screens/OnboardingScreen.jsx
+++ src/screens/OnboardingScreen.jsx
@@ -1,22 +1,47 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, FlatList } from 'react-native'
 import React from 'react'
 import SplashLayoutComponent from '../components/splash/SplashLayoutComponent'
 import { splashData } from '../../constants/data'
+import { useNavigation } from '@react-navigation/native'
 const OnboardingScreen = () => {
-    const [id, setId]  = React.useState(0);
-    React.useEffect(()=>{
-        setInterval(()=>{
-            setId((prevId) => (prevId < 3 ? prevId + 1 : 0));
-        }, 1000);
-        
-    },[])
-  return (
-    <>
-        {/* first design the splash screen */}
-        {/* Next we iterate through the screens */}
+  const navigation = useNavigation();
+  const slideRef = React.useRef(null);
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+  
+  const viewableItemsChanged = React.useRef(({ viewableItems }) => {
+    setCurrentIndex(viewableItems[0].index);
+  }).current
+  const viewConfig = React.useRef({ viewAreaCoveragePercentThreshold: 50 }).current
+  
+  const next = () => {
+    if (currentIndex < splashData.length - 1) {
+      slideRef.current.scrollToIndex({ index: currentIndex + 1 })
+      
+    }else{
+      navigation.navigate('GetYouIn')
+    }
+  }
+  
+  const skip = () =>{
+    
+    navigation.navigate('GetYouIn')
+  }
 
-        <SplashLayoutComponent splashData={splashData[id]} />
-    </>
+  return (
+        
+        <FlatList
+          data={splashData}
+          renderItem={({item})=><SplashLayoutComponent splashData={item} skip={skip} navigation={navigation} next={next}  />} 
+          keyExtractor={(item)=> item.id}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          onViewableItemsChanged={viewableItemsChanged}
+          viewabilityConfig={viewConfig}
+          ref={slideRef}
+        />
+            
   )
 }
 
