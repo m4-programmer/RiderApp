5b5d19cca70746dcd89b65e4bda72882d4a3382d
Adolphus Miracle <miraboy13@gmail.com>
2023-12-31 20:59:57 +0100

    feat: created landing page for Signup called GetYouIn

---
+++ src/screens/SignUp/GetYouInScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GetYouInScreen = () => {
+  return (
+    <View>
+      <Text>GetYouInScreen</Text>
+    </View>
+  )
+}
+
+export default GetYouInScreen
+
+const styles = StyleSheet.create({})
