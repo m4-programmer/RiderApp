00327ecb7bf27f914cc5a34e306aeb32abba3bb9
Adolphus Miracle <miraboy13@gmail.com>
2023-12-31 21:00:39 +0100

    fix: updated logic and added logic to control footer buttons

--- src/components/splash/FooterComponent.jsx
+++ src/components/splash/FooterComponent.jsx
@@ -2,19 +2,20 @@ import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { COLORS, appMode, icons } from '../../../constants/Index'
 
-const FooterComponent = ({id, mode}) => {
+const FooterComponent = ({id, mode,skip, next}) => {
+  
   return (
     <View className='justify-between items-center flex-row  mx-5' >
 
         <View className='flex-col justify-evenly items-start mt-2'>
             <Image source={id % 2 !== 0 ? icons.Navigation : icons.NavigationOrange} className='h-23 w-23 mb-2' resizeMode='contain' />
-            <TouchableOpacity onPress={()=>{}}>
+            <TouchableOpacity onPress={skip}>
                 <Text className='text-lg mt-2' style={{ color: mode === appMode.dark ? COLORS.textDark : COLORS.lightMode }}>Skip</Text>
             </TouchableOpacity>
         </View>
 
 
-        <TouchableOpacity className='' onPress={()=>{}}>
+        <TouchableOpacity className='' onPress={next}>
             <Image source={ id % 2 !== 0 ? icons.Loader2 : icons.Loader} className='w-20 h-20' resizeMode='contain' />
         </TouchableOpacity>
     </View>
