08431e165f172f140ef9a2dcc825e23a8595de2d
Adolphus Miracle <miraboy13@gmail.com>
2024-01-04 18:32:12 +0100

    refactor: registered newly created components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,8 +2,9 @@ import HeadingComponent from "./splash/HeadingComponent";
 import SplashLayoutComponent from "./splash/SplashLayoutComponent";
 import FooterComponent from "./splash/FooterComponent";
 import CustomButton from "./buttons/CustomButton";
+import CustomTextInput from "./formInputs/CustomTextInput";
 
-export {CustomButton}
+export {CustomButton, CustomTextInput}
 
 export default {
     HeadingComponent,
