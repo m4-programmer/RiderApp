8701a971d838f8aba003876dd811ce2c6e649b2a
Adolphus Miracle <miraboy13@gmail.com>
2024-01-04 17:38:26 +0100

    feat: created custom button component

---
+++ src/components/buttons/CustomButton.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { COLORS, typography } from '../../../constants/theme';
+
+const CustomButton = ({text, textColor, bgColor=COLORS.primary3, onPressed}) => {
+  return (
+    <TouchableOpacity style={[styles.container,{backgroundColor: bgColor}]} onPress={onPressed}>
+      <Text style={[typography.body1('dark'), styles.text,{color: textColor}]}>{text}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default CustomButton
+
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 50,
+        alignItems: 'center',
+        padding: hp(2),
+        marginVertical: 7,
+    },
+    text: {
+        fontWeight: '500'
+    }
+})
