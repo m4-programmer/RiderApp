1d71242215144fdad8b076e1150c74d261715d7e
Adolphus Miracle <miraboy13@gmail.com>
2024-01-04 19:24:04 +0100

    refactor: defined new icons

--- constants/icons.js
+++ constants/icons.js
@@ -4,5 +4,6 @@ import Loader2 from '../assets/Icons/loader/Loader2.png'
 import Navigation from '../assets/Icons/navigation/Navigation.png'
 import NavigationOrange from '../assets/Icons/navigation/NavigationOrange.png'
 import Navigation2x from '../assets/Icons/navigation/Navigation2x.png'
-
-export default {Loader, Loader2, Navigation, NavigationOrange, Navigation2x}
+import Board from '../assets/Icons/board.png'
+import User from '../assets/Icons/user.png'
+export default {Loader, Loader2, Navigation, NavigationOrange, Navigation2x, Board, User}
