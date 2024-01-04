a09211c922f59cd1d4d4bfa5013e9bdd4a3fd8e7
Adolphus Miracle <miraboy13@gmail.com>
2024-01-04 19:23:33 +0100

    feat: created a helper component for controlling Margin

---
+++ src/components/helper/Margin.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Margin = ({horizontal,vertical}) => {
+  return (
+    <View style={{marginHorizontal: horizontal, marginVertical: vertical}}>
+      
+    </View>
+  )
+}
+
+export default Margin
