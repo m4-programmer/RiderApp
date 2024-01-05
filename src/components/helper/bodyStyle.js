ed47489934c11f646602dae71f076aa807a05f7a
Adolphus Miracle <miraboy13@gmail.com>
2024-01-05 23:56:21 +0100

    feat: created a reuseable color styleSheet

---
+++ src/components/helper/bodyStyle.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../../constants/theme";
+
+const generalStyle = StyleSheet.create({
+    body:{
+        backgroundColor: COLORS.darkMode,
+        flex:1
+    }
+});
+
+export default generalStyle
