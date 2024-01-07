4b1c2e060c5254a6b7073d862b67ec9f9ebca5ec
Adolphus Miracle <miraboy13@gmail.com>
2024-01-07 06:38:26 +0100

    feat: created screen for verify account

---
+++ src/screens/SignUp/VerifyAccount.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const VerifyAccount = () => {
+  return (
+    <View>
+      <Text>VerifyAccount</Text>
+    </View>
+  )
+}
+
+export default VerifyAccount
+
+const styles = StyleSheet.create({})
