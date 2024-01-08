71836b42d6a9d7887a6447150344bf5a90f67df2
Adolphus Miracle <miraboy13@gmail.com>
2024-01-08 23:01:20 +0100

    feat: created stacks for verify account and upload picture screns

--- src/navigation/Index.jsx
+++ src/navigation/Index.jsx
@@ -4,7 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import WelcomeScreen from '../screens/WelcomeScreen'
 import OnboardingScreen from '../screens/OnboardingScreen'
 import GetYouInScreen from '../screens/SignUp/GetYouInScreen'
-import { LoginScreen, SignupScreen } from '../screens'
+import { LoginScreen, SignupScreen, UploadPictureScreen, VerifyAccount } from '../screens'
 
 const Stack = createNativeStackNavigator()
 
@@ -13,10 +13,12 @@ const AppNavigation = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Welcome' screenOptions={{headerShown: false}}>
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{gestureEnabled: false, gestureDirection: 'horizontal'}} />
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{gestureEnabled: false, gestureDirection: 'horizontal',}} />
         <Stack.Screen name="GetYouIn" component={GetYouInScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} screenOptions={{headerShown: true}} />
+        <Stack.Screen name="Signup" component={SignupScreen} screenOptions={{headerShown: false}}  />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="VerifyAccount" component={VerifyAccount} />
+        <Stack.Screen name="UploadPicture" component={UploadPictureScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
