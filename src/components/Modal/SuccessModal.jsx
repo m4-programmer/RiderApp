64a48d4b9b47b0bf7785cd6070c7af79c85d22a1
Adolphus Miracle <miraboy13@gmail.com>
2024-01-14 23:58:18 +0100

    feat: created component to handle success modal

---
+++ src/components/Modal/SuccessModal.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SuccessModal() {
+  return (
+    <View>
+      <Text>SuccessModal</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
