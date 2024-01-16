ac83ac51d3f7a1f7dbf0226d1a1ca19a658fdc66
Adolphus Miracle <miraboy13@gmail.com>
2024-01-16 23:12:00 +0100

    feat: created new screens for onboarding

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -2,4 +2,7 @@ import SignupScreen from "./SignUp/SignupScreen";
 import LoginScreen from "./SignIn/LoginScreen";
 import UploadPictureScreen from "./SignUp/UploadPictureScreen";
 import VerifyAccount from "./SignUp/VerifyAccount";
-export {SignupScreen, LoginScreen, VerifyAccount, UploadPictureScreen}
+import LandingScreen from "./Onboarding/LandingScreen";
+import OnboardingChoiceScreen from "./Onboarding/OnboardingChoiceScreen";
+
+export {SignupScreen, LoginScreen, VerifyAccount, UploadPictureScreen, LandingScreen, OnboardingChoiceScreen}
