d5be609c879f4727ecdb4eb26d8ef2c08b8e232d
Adolphus Miracle <miraboy13@gmail.com>
2024-01-18 23:53:50 +0100

    feat: created screens needed for the bottom tab

---
+++ src/screens/BottomTabs/HomeScreen.jsx
@@ -0,0 +1,15 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { generalStyle } from '../../components'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={generalStyle.body}>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
